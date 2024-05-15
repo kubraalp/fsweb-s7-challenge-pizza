f5c728cd60b83f5b57988727c07cf674b0ca0283
Kübra Alp <kubra.alp.k@gmail.com>
2024-05-15 23:34:38 +0300

    Update OrderForm.js

--- src/pages/OrderForm.js
+++ src/pages/OrderForm.js
@@ -1,16 +1,32 @@
 import "./OrderForm.css";
 import axios from "axios";
 
-<div>
-  <h1>Position Absolute Acı Pizza</h1>
-  <div>
-    <p>
-      Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza
-      tam sana göre.Pizza, domates, peynir ve genellikle çeşitli diğer
-      malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir
-      fırında yüksek sıcaklıkta pişirilen genellikle yuvarlak, düzleştirilmiş
-      mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezetli bir
-      yemektir..Küçük bir pizzaya bazen pizzetta denir.
-    </p>
-  </div>
-</div>;
+return (
+  <section>
+    <header>
+      <div>
+        <p>Anasayfa- Seçenekler Sipariş Oluştur</p>
+      </div>
+    </header>
+    <div>
+      <h2>Position Absolute Acı Pizza</h2>
+      <div>
+        <div>
+          <p>85.50TL</p>
+          <div>
+            <p>4.9</p>
+            <p>(200)</p>
+          </div>
+        </div>
+        <p>
+          Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı
+          pizza tam sana göre.Pizza, domates, peynir ve genellikle çeşitli diğer
+          malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir
+          fırında yüksek sıcaklıkta pişirilen genellikle yuvarlak,
+          düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli
+          lezetli bir yemektir..Küçük bir pizzaya bazen pizzetta denir.
+        </p>
+      </div>
+    </div>
+  </section>
+);
