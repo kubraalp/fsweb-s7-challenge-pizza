e4e0f0dc0f3aa0e9490978064a199964dbe4bc6b
Kübra Alp <kubra.alp.k@gmail.com>
2024-05-15 23:17:43 +0300

    Update OrderForm.js

---
+++ src/pages/OrderForm.js
@@ -0,0 +1,16 @@
+import "./OrderForm.css";
+import axios from "axios";
+
+<div>
+  <h1>Position Absolute Acı Pizza</h1>
+  <div>
+    <p>
+      Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza
+      tam sana göre.Pizza, domates, peynir ve genellikle çeşitli diğer
+      malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir
+      fırında yüksek sıcaklıkta pişirilen genellikle yuvarlak, düzleştirilmiş
+      mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezetli bir
+      yemektir..Küçük bir pizzaya bazen pizzetta denir.
+    </p>
+  </div>
+</div>;
